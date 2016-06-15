25508e83c0691637dc1e3f6ffa17f9e86b154138
RuXD <rudyhalim.microsoft@gmail.com>
2016-06-15 14:04:23 +0700

    update query tables

--- dbquery.sql
+++ dbquery.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jun 12, 2016 at 02:08 PM
+-- Generation Time: Jun 15, 2016 at 02:01 PM
 -- Server version: 5.7.12
 -- PHP Version: 5.5.35
 
@@ -17,10 +17,10 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tbl_carts`
+-- Table structure for table `carts`
 --
 
-CREATE TABLE `tbl_carts` (
+CREATE TABLE `carts` (
   `cart_id` int(11) UNSIGNED NOT NULL,
   `user_id` int(11) NOT NULL,
   `product_id` int(11) NOT NULL,
@@ -31,54 +31,57 @@ CREATE TABLE `tbl_carts` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tbl_configs`
+-- Table structure for table `configs`
 --
 
-CREATE TABLE `tbl_configs` (
+CREATE TABLE `configs` (
   `config_id` int(1) UNSIGNED NOT NULL,
   `servicecharge_currency` varchar(3) NOT NULL COMMENT '3 digits following ISO 4217 Currency Codes',
   `servicecharge_value` decimal(9,2) NOT NULL COMMENT 'to be displayed at the confirmation page adding to total amount charged'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `tbl_configs`
+-- Dumping data for table `configs`
 --
 
-INSERT INTO `tbl_configs` (`config_id`, `servicecharge_currency`, `servicecharge_value`) VALUES
+INSERT INTO `configs` (`config_id`, `servicecharge_currency`, `servicecharge_value`) VALUES
 (1, 'SGD', '1.25');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tbl_countries`
+-- Table structure for table `countries`
 --
 
-CREATE TABLE `tbl_countries` (
+CREATE TABLE `countries` (
   `country_id` int(3) UNSIGNED NOT NULL,
   `country_name` varchar(30) NOT NULL,
   `country_currency` varchar(3) NOT NULL COMMENT '3 digits following ISO 4217 Currency Codes',
   `country_trade` enum('Y','N') DEFAULT 'Y' COMMENT 'open for trade currency or not',
-  `markup_value` decimal(9,2) NOT NULL COMMENT 'markup for each currency code'
+  `markup_value` decimal(9,2) NOT NULL COMMENT 'markup for each currency code following the country code'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='This data table also used for user''s country registration';
 
 --
--- Dumping data for table `tbl_countries`
+-- Dumping data for table `countries`
 --
 
-INSERT INTO `tbl_countries` (`country_id`, `country_name`, `country_currency`, `country_trade`, `markup_value`) VALUES
+INSERT INTO `countries` (`country_id`, `country_name`, `country_currency`, `country_trade`, `markup_value`) VALUES
 (1, 'Singapore', 'SGD', 'Y', '0.11'),
 (2, 'Malaysia', 'MYR', 'Y', '0.12'),
 (3, 'Thailand', 'THB', 'Y', '0.13'),
 (4, 'Vietnam', 'VND', 'Y', '0.14'),
-(5, 'Indonesia', 'IDR', 'N', '0.15');
+(5, 'Indonesia', 'IDR', 'N', '0.15'),
+(6, 'China', 'RMB', 'N', '0.16'),
+(7, 'India', 'INR', 'N', '0.17'),
+(8, 'Myanmar', 'MMK', 'N', '0.18');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tbl_orders`
+-- Table structure for table `orders`
 --
 
-CREATE TABLE `tbl_orders` (
+CREATE TABLE `orders` (
   `order_id` int(11) UNSIGNED NOT NULL,
   `invoice_no` varchar(50) NOT NULL,
   `user_id` int(11) NOT NULL,
@@ -89,10 +92,10 @@ CREATE TABLE `tbl_orders` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tbl_order_items`
+-- Table structure for table `order_items`
 --
 
-CREATE TABLE `tbl_order_items` (
+CREATE TABLE `order_items` (
   `orderitem_id` int(11) UNSIGNED NOT NULL,
   `invoice_no` varchar(50) NOT NULL,
   `seller_id` int(11) NOT NULL,
@@ -106,19 +109,19 @@ CREATE TABLE `tbl_order_items` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tbl_order_status`
+-- Table structure for table `order_status`
 --
 
-CREATE TABLE `tbl_order_status` (
+CREATE TABLE `order_status` (
   `orderstatus_id` int(2) UNSIGNED NOT NULL,
   `orderstatus_name` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `tbl_order_status`
+-- Dumping data for table `order_status`
 --
 
-INSERT INTO `tbl_order_status` (`orderstatus_id`, `orderstatus_name`) VALUES
+INSERT INTO `order_status` (`orderstatus_id`, `orderstatus_name`) VALUES
 (1, 'pending'),
 (2, 'processing'),
 (3, 'completed');
@@ -126,10 +129,10 @@ INSERT INTO `tbl_order_status` (`orderstatus_id`, `orderstatus_name`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tbl_pages`
+-- Table structure for table `pages`
 --
 
-CREATE TABLE `tbl_pages` (
+CREATE TABLE `pages` (
   `page_id` int(5) UNSIGNED NOT NULL,
   `url_addr` varchar(225) NOT NULL,
   `page_title` varchar(225) NOT NULL,
@@ -144,10 +147,10 @@ CREATE TABLE `tbl_pages` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tbl_products`
+-- Table structure for table `products`
 --
 
-CREATE TABLE `tbl_products` (
+CREATE TABLE `products` (
   `product_id` int(11) UNSIGNED NOT NULL,
   `seller_id` int(11) NOT NULL COMMENT 'seller''s user id',
   `seller_currency` varchar(3) NOT NULL COMMENT '3 digits following ISO 4217 Currency Codes',
@@ -160,19 +163,19 @@ CREATE TABLE `tbl_products` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tbl_roles`
+-- Table structure for table `roles`
 --
 
-CREATE TABLE `tbl_roles` (
+CREATE TABLE `roles` (
   `role_id` int(3) UNSIGNED NOT NULL,
   `role_name` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `tbl_roles`
+-- Dumping data for table `roles`
 --
 
-INSERT INTO `tbl_roles` (`role_id`, `role_name`) VALUES
+INSERT INTO `roles` (`role_id`, `role_name`) VALUES
 (1, 'Super Admin'),
 (2, 'Regular Customer'),
 (3, 'Special Customer');
@@ -180,10 +183,10 @@ INSERT INTO `tbl_roles` (`role_id`, `role_name`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tbl_users`
+-- Table structure for table `users`
 --
 
-CREATE TABLE `tbl_users` (
+CREATE TABLE `users` (
   `user_id` int(11) UNSIGNED NOT NULL,
   `email_addr` varchar(100) NOT NULL,
   `passwd` varchar(225) NOT NULL,
@@ -197,10 +200,10 @@ CREATE TABLE `tbl_users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `tbl_users`
+-- Dumping data for table `users`
 --
 
-INSERT INTO `tbl_users` (`user_id`, `email_addr`, `passwd`, `first_name`, `last_name`, `gender`, `phone_no`, `display_image`, `country_id`, `role_id`) VALUES
+INSERT INTO `users` (`user_id`, `email_addr`, `passwd`, `first_name`, `last_name`, `gender`, `phone_no`, `display_image`, `country_id`, `role_id`) VALUES
 (1, 'rudyhalim.microsoft@gmail.com', '81dc9bdb52d04dc20036dbd8313ed055', 'Rudy', 'Halim', 'male', '085780886425', NULL, 5, 1);
 
 --
@@ -208,63 +211,64 @@ INSERT INTO `tbl_users` (`user_id`, `email_addr`, `passwd`, `first_name`, `last_
 --
 
 --
--- Indexes for table `tbl_carts`
+-- Indexes for table `carts`
 --
-ALTER TABLE `tbl_carts`
+ALTER TABLE `carts`
   ADD PRIMARY KEY (`cart_id`);
 
 --
--- Indexes for table `tbl_configs`
+-- Indexes for table `configs`
 --
-ALTER TABLE `tbl_configs`
+ALTER TABLE `configs`
   ADD PRIMARY KEY (`config_id`);
 
 --
--- Indexes for table `tbl_countries`
+-- Indexes for table `countries`
 --
-ALTER TABLE `tbl_countries`
-  ADD PRIMARY KEY (`country_id`);
+ALTER TABLE `countries`
+  ADD PRIMARY KEY (`country_id`),
+  ADD UNIQUE KEY `country_name` (`country_name`,`country_currency`);
 
 --
--- Indexes for table `tbl_orders`
+-- Indexes for table `orders`
 --
-ALTER TABLE `tbl_orders`
+ALTER TABLE `orders`
   ADD PRIMARY KEY (`order_id`);
 
 --
--- Indexes for table `tbl_order_items`
+-- Indexes for table `order_items`
 --
-ALTER TABLE `tbl_order_items`
+ALTER TABLE `order_items`
   ADD PRIMARY KEY (`orderitem_id`);
 
 --
--- Indexes for table `tbl_order_status`
+-- Indexes for table `order_status`
 --
-ALTER TABLE `tbl_order_status`
+ALTER TABLE `order_status`
   ADD PRIMARY KEY (`orderstatus_id`);
 
 --
--- Indexes for table `tbl_pages`
+-- Indexes for table `pages`
 --
-ALTER TABLE `tbl_pages`
+ALTER TABLE `pages`
   ADD PRIMARY KEY (`page_id`);
 
 --
--- Indexes for table `tbl_products`
+-- Indexes for table `products`
 --
-ALTER TABLE `tbl_products`
+ALTER TABLE `products`
   ADD PRIMARY KEY (`product_id`);
 
 --
--- Indexes for table `tbl_roles`
+-- Indexes for table `roles`
 --
-ALTER TABLE `tbl_roles`
+ALTER TABLE `roles`
   ADD PRIMARY KEY (`role_id`);
 
 --
--- Indexes for table `tbl_users`
+-- Indexes for table `users`
 --
-ALTER TABLE `tbl_users`
+ALTER TABLE `users`
   ADD PRIMARY KEY (`user_id`);
 
 --
@@ -272,52 +276,52 @@ ALTER TABLE `tbl_users`
 --
 
 --
--- AUTO_INCREMENT for table `tbl_carts`
+-- AUTO_INCREMENT for table `carts`
 --
-ALTER TABLE `tbl_carts`
+ALTER TABLE `carts`
   MODIFY `cart_id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
 --
--- AUTO_INCREMENT for table `tbl_configs`
+-- AUTO_INCREMENT for table `configs`
 --
-ALTER TABLE `tbl_configs`
+ALTER TABLE `configs`
   MODIFY `config_id` int(1) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
--- AUTO_INCREMENT for table `tbl_countries`
+-- AUTO_INCREMENT for table `countries`
 --
-ALTER TABLE `tbl_countries`
-  MODIFY `country_id` int(3) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+ALTER TABLE `countries`
+  MODIFY `country_id` int(3) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 --
--- AUTO_INCREMENT for table `tbl_orders`
+-- AUTO_INCREMENT for table `orders`
 --
-ALTER TABLE `tbl_orders`
+ALTER TABLE `orders`
   MODIFY `order_id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
 --
--- AUTO_INCREMENT for table `tbl_order_items`
+-- AUTO_INCREMENT for table `order_items`
 --
-ALTER TABLE `tbl_order_items`
+ALTER TABLE `order_items`
   MODIFY `orderitem_id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
 --
--- AUTO_INCREMENT for table `tbl_order_status`
+-- AUTO_INCREMENT for table `order_status`
 --
-ALTER TABLE `tbl_order_status`
+ALTER TABLE `order_status`
   MODIFY `orderstatus_id` int(2) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
--- AUTO_INCREMENT for table `tbl_pages`
+-- AUTO_INCREMENT for table `pages`
 --
-ALTER TABLE `tbl_pages`
+ALTER TABLE `pages`
   MODIFY `page_id` int(5) UNSIGNED NOT NULL AUTO_INCREMENT;
 --
--- AUTO_INCREMENT for table `tbl_products`
+-- AUTO_INCREMENT for table `products`
 --
-ALTER TABLE `tbl_products`
+ALTER TABLE `products`
   MODIFY `product_id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
 --
--- AUTO_INCREMENT for table `tbl_roles`
+-- AUTO_INCREMENT for table `roles`
 --
-ALTER TABLE `tbl_roles`
+ALTER TABLE `roles`
   MODIFY `role_id` int(3) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
--- AUTO_INCREMENT for table `tbl_users`
+-- AUTO_INCREMENT for table `users`
 --
-ALTER TABLE `tbl_users`
+ALTER TABLE `users`
   MODIFY `user_id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
